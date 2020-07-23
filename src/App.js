45dddd859f51b3f9135e63348a240c4c55d143f9
Krikor Ailanjian <kailanjian@gmail.com>
2020-07-23 15:12:33 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function getClue() {
+  return fetch('http://jservice.io/api/random');
+}
+
 function App() {
+  const [clue, setClue] = useState({
+    question: "something",
+    answer: "what is something?",
+    category: "Potent Potables"
+  })
+
+  const nextClue = () => {
+    getClue().then((clue) => {
+      console.log(clue);
+      clue.json().then((response) => {
+        console.log(response[0]);
+        const generatedClue = response[0];
+        debugger;
+        setClue({
+          question: generatedClue.question,
+          answer: generatedClue.answer,
+          category: generatedClue.category.title
+        })
+      });
+    });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        Jeopardy
+      </div>
+      <div>
+        Category: {clue.category}
+      </div>
+      <div>
+        Answer: {clue.question}
+      </div>
+      <div>
+        What is?: {clue.answer}
+      </div>
+      <button onClick={nextClue}>Get Clue</button>
     </div>
   );
 }
