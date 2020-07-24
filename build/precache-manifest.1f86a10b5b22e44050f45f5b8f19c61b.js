765e03ae048505776a97f2be99f3bb79ab2685db
Krikor Ailanjian <krikor.ailanjian@appfolio.com>
2020-07-24 11:23:55 -0700

    add build files for github pages

---
+++ build/precache-manifest.1f86a10b5b22e44050f45f5b8f19c61b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71ced86ed2938da319e4b172a201cc84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d05cdfacf7a55b4d8c1",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "e1ab3fd0619f2bc27afd",
+    "url": "/static/css/main.9668de5b.chunk.css"
+  },
+  {
+    "revision": "7d05cdfacf7a55b4d8c1",
+    "url": "/static/js/2.0d3dd711.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0d3dd711.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1ab3fd0619f2bc27afd",
+    "url": "/static/js/main.1a51d9ce.chunk.js"
+  },
+  {
+    "revision": "67f0f92a3acfcca57b92",
+    "url": "/static/js/runtime-main.8835df34.js"
+  }
+]);
