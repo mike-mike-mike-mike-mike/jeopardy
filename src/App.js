49cef319b5dabfb5698c9f7706f6af601c8f13a6
Krikor Ailanjian <krikor.ailanjian@appfolio.com>
2020-07-24 11:13:06 -0700

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -10,15 +10,16 @@ function JeopardyCard(props) {
     category,
     question,
     answer,
+    value,
     showAnswer
   } = props;
 
   return (
     <div>
-      <div className="Jeopardy-box flex-box-column" style={{"height": "5em"}}>
-        <div style={{"font-size": "2em", "text-shadow": "2px 2px 0px black", "font-weight": "bold"}}>{category.toUpperCase()}</div>
+      <div className="Jeopardy-box flex-box-column" style={{"height": "7em"}}>
+        <div style={{"font-size": "2em", "text-shadow": "2px 2px 0px black", "font-weight": "bold"}}>{category.toUpperCase()} ({value})</div>
       </div>
-      <div className="Jeopardy-box flex-box-column" style={{"height": "15em"}}>
+      <div className="Jeopardy-box flex-box-column" style={{"height": "18em", "overflow-y": "auto"}}>
         <div style={{"font-size": "2em", "padding": "0.5em"}}>{showAnswer ? answer : question}</div>
       </div>
     </div>
@@ -27,16 +28,29 @@ function JeopardyCard(props) {
 
 
 function App() {
+  const START = 0;
+  const QUESTION = 1;
+  const GUESSED = 2;
+
   const [clue, setClue] = useState({
     question: "...",
     answer: "...",
     category: "...",
+    value: "",
     showAnswer: false
   })
   const [guess, setGuess] = useState('');
+  const [message, setMessage] = useState('Welcome to Jeopardy!');
+  const [points, setPoints] = useState(0);
+  const [gameState, setGameState] = useState(START);
 
+  const addPoints = (amount) => {
+    setPoints(points + amount);
+  }
 
   const nextClue = () => {
+    setGuess("");
+    setGameState(QUESTION);
     getClue().then((clue) => {
       clue.json().then((response) => {
         const generatedClue = response[0];
@@ -44,41 +58,49 @@ function App() {
           question: generatedClue.question,
           answer: generatedClue.answer,
           category: generatedClue.category.title,
+          value: generatedClue.value,
           showAnswer: false
         })
+        setMessage(`"${generatedClue.category.title}" for ${generatedClue.value}...`)
       });
     });
   }
 
   const initializeClue = () => {
-    getClue().then((clue) => {
-      return clue.json()
-    }).then((response) => {
-        const generatedClue = response[0];
-        setClue({
-          question: generatedClue.question,
-          answer: generatedClue.answer,
-          category: generatedClue.category.title,
-          showAnswer: false
-        })
-    });
+    nextClue();
   }
 
   const handleGuess = () => {
-    console.log(guess)
-    if (guess.toLowerCase() === clue.answer.toLowerCase()) {
-      alert('correct');
+    setGameState(GUESSED);
+    if (isAnswerCorrect()) {
+      setMessage("Right!")
+      addPoints(clue.value)
     } else {
-      alert('wrong');
+      setMessage("Wrong!")
+      addPoints(clue.value * -1);
     }
   }
 
-  if (clue.question === '...') {
+  const handleCorrectOverride = () => {
+    if (!isAnswerCorrect()) {
+      setMessage("Incorrectly marked as wrong... points added back to your score!")
+      addPoints(2 * clue.value);
+      setGuess(clue.answer);
+    }
+  }
+
+  if (gameState === START) {
+    setGameState(QUESTION);
     initializeClue();
   }
 
-  const showAnswer = () => {
-    setClue({...clue, showAnswer: true})
+  const toggleShowAnswer = () => {
+    console.log("showing answer");
+    setClue({...clue, showAnswer: !clue.showAnswer})
+  }
+
+  const isAnswerCorrect = () => {
+    return guess.toLowerCase() === clue.answer.toLowerCase();
   }
 
   return (
@@ -87,17 +109,35 @@ function App() {
         Jeopardy
       </div>
       <div>
-        <JeopardyCard category={clue.category} question={clue.question} answer={clue.answer} showAnswer={clue.showAnswer}/>
+        <JeopardyCard value={clue.value} category={clue.category} question={clue.question} answer={clue.answer} showAnswer={clue.showAnswer}/>
       </div>
       <div className='form-group form-inline mt-4'>
-        <form>
-          <input className='form-control' value={guess} onChange={(event) => {setGuess(event.target.value)}} type="text" />
-          <button className='btn btn-primary' onClick={handleGuess}>Guess</button>
+        <form onSubmit={e => e.preventDefault()}>
+          <input disabled={gameState === GUESSED} className='form-control' value={guess} onChange={(event) => {setGuess(event.target.value)}} type="text" />
+          <button disabled={gameState === GUESSED} className='btn btn-primary mr-2' onClick={handleGuess}>Guess</button>
+          {
+            gameState === GUESSED && !isAnswerCorrect() &&
+            <button onClick={handleCorrectOverride} className='btn btn-secondary' >I was right!</button>
+          }
         </form>
       </div>
       <div>
-        <button onClick={showAnswer}>Show Answer</button>
-        <button onClick={nextClue}>Get Clue</button>
+        <button className='btn btn-secondary mr-2' type="button" onClick={toggleShowAnswer}>
+          {
+            !clue.showAnswer ? "Show Answer" : "Show Question"
+          }
+        </button>
+        <button className='btn btn-secondary' type="button" onClick={nextClue}>
+          {
+            gameState === QUESTION ? "Skip Clue" : "Next Clue"
+          }
+        </button>
+      </div>
+      <div>
+        <p className='mt-4 h4'>{message}</p>
+      </div>
+      <div>
+        <p className='mt-4 h4'>You have: {points} points</p>
       </div>
     </div>
   );
